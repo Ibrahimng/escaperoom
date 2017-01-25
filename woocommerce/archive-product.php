f89ebb3bc62f182d524e40dc5823ba1c0eb86bce
FarrukIslam <farrukislam1@gmail.com>
2017-01-25 19:56:35 +0600

    working archive page and tab layout

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package escaperoom
+ */
+
+get_header(); ?>
+	
+	<div id="primary" class="container">
+		<main id="main" class="site-main" role="main">
+
+			<div class="row">
+				<?php	
+				if ( is_active_sidebar( 'vendor-sidebar' ) ) {
+				$column = 'col-md-9';
+				} else {
+					$column = 'col-md-12';
+				}
+				if ( is_active_sidebar( 'vendor-sidebar' ) ) : ?>		
+
+				<div class="col-md-3">
+					<div class="sidebar">
+						<?php dynamic_sidebar('vendor-sidebar'); ?>
+					</div>
+				</div>
+				<?php endif; ?>
+			
+		 		<div class="<?php echo $column; ?>">
+		 			<div class="row">
+						<ul class="escaperoom_pro_tab pull-right">
+						  <li class="active">
+						 	 <a class="btn btn-primary" data-toggle="tab" href="#grid">
+						 	 <span class="ion-android-apps"></span>Grid
+						 	 </a>
+						  </li>
+						  <li>
+						  	<a class="btn btn-primary" data-toggle="tab" href="#list">
+						  	<span class="ion-navicon"></span>List
+						  	</a>
+						  </li>
+						  <li>
+						  	<a class="btn btn-primary" data-toggle="tab" href="#map">
+						  	<span class="ion-ios-location-outline"></span>Map
+						  	</a>
+						  </li>
+						</ul>
+					</div>
+					<div class="row">
+						<div class="tab-content">
+
+							<div id="grid" class="tab-pane fade in active">
+							<?php 
+								while ( have_posts() ) : the_post();
+									wc_get_template_part( 'content', 'product' );
+								endwhile;  
+do_action( 'woocommerce_after_shop_loop' );
+							?>
+							
+							</div>
+
+							<div id="list" class="tab-pane fade">
+							<?php 
+								while ( have_posts() ) : the_post(); 
+									wc_get_template_part( 'contentlist', 'product' );
+								endwhile; 
+								do_action( 'woocommerce_after_shop_loop' );
+							 ?>
+							</div>
+
+							<div id="map" class="tab-pane fade">
+							    <div id="product_map"></div>
+							</div>
+
+						</div>
+					</div>	
+				</div><!--#col-->
+			</div><!--#row-->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ woocommerce/contentlist-product.php
@@ -0,0 +1,52 @@
+<?php 
+			
+							global $product, $woocommerce, $store_info; 
+							$author = get_user_by('id', $post->post_author);
+							$store_info = dokan_get_store_info( $author->ID );
+						?>
+							<div class="col-md-12">
+
+							 	<div class="list_escaperoom">
+
+							 		<div class="col-md-3">
+							 		<?php the_post_thumbnail('pro_thum_shop',array ('class' => 'list_esc_img')) ?>
+							 		</div>
+
+							 		<div class="col-md-7">
+										<h2 class="list_pro_tile">
+										<?php the_title(); ?>
+										</h2>
+										<div class="list_author">
+											<a href="<?php echo dokan_get_store_url( $author->ID );?>">
+											<?php echo get_avatar( $author->ID, 50 ); ?>
+											<h5><?php echo esc_html( $store_info['store_name'] ); ?></h5>
+											</a>
+										</div>
+										<div class="list_price_section hidden-md listmobile">
+											<span class="list_price">
+											<?php echo get_woocommerce_currency_symbol(); 
+											echo $product->get_price(); ?>
+											</span>
+											<br>
+											<span class="list_duction">
+												<?php echo $product->get_duration_unit();?>
+											</span>
+										</div>
+									</div>
+
+									<div class="col-md-2 hidden-xs hidden-sm">	
+										<div class="list_price_section pull-right">
+											<span class="list_price">
+											<?php echo get_woocommerce_currency_symbol(); 
+											echo $product->get_price(); ?>
+											</span>
+											<br>
+											<span class="list_duction">
+												<?php echo $product->get_duration_unit();?>
+											</span>
+										</div>
+									</div>	
+
+							 	</div>
+							</div> 
+			
